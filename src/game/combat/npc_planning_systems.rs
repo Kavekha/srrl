cd54ed0d1b550dac4d17255dac58c55adeee8bf9
Kavekha <edelamaranthid@netcourrier.com>
2023-07-06 00:25:56 +0200

    basic planning, NPC kick ass...
    Player need to die now!

--- src/game/combat/npc_planning_systems.rs
+++ src/game/combat/npc_planning_systems.rs
@@ -2,9 +2,9 @@ use std::collections::VecDeque;
 
 use bevy::prelude::*;
 
-use crate::{game::{pieces::components::{Npc, Occupier}, tileboard::components::BoardPosition, player::Player}, vectors::{Vector2Int, find_path}, map_builders::map::Map};
+use crate::{game::{pieces::components::{Npc, Occupier}, tileboard::components::BoardPosition, player::Player, combat::AP_COST_MELEE}, vectors::{Vector2Int, find_path}, map_builders::map::Map};
 
-use super::{components::{CombatInfos, ActionPoints}, events::{EntityEndTurnEvent, Turn, EntityTryMoveEvent}};
+use super::{components::{CombatInfos, ActionPoints}, events::{EntityEndTurnEvent, Turn, EntityTryMoveEvent}, AP_COST_MOVE};
 
 
 /// NPC : Generate / Choice to forfeit their turn.
@@ -39,13 +39,22 @@ pub fn npc_planning(
     board: Res<Map>,
     mut ev_try_move: EventWriter<EntityTryMoveEvent>
 ) {
+    println!("Planing: First step:");
+    
     let Some(entity) = combat_info.current_entity else { return };
     let Ok(entity_infos) = query_npc.get(entity) else { return };
     let (npc_action_points, npc_position) = entity_infos;
     let Ok(player_position) = query_player.get_single() else { return };
    
-    // Can see player?
-    // Path to player?
+    println!("Planing: Check: OK for {:?}", entity);
+
+    // Can do something?
+    if npc_action_points.current < AP_COST_MELEE { 
+        ev_endturn.send(EntityEndTurnEvent {entity}); 
+        return
+    }
+    println!("Planing: {:?} Has enough AP : {:?}", entity, npc_action_points.current);
+
     let path_to_player = find_path(
         npc_position.v,
         player_position.v, 
@@ -53,24 +62,41 @@ pub fn npc_planning(
         &query_occupied.iter().map(|p| p.v).collect(),
         true,
     );
-
-    let mut forfeit = false;
-
-    // Impossible d'atteindre le joueur.
     if path_to_player.is_none() { 
-        forfeit = true;
+        ev_endturn.send(EntityEndTurnEvent {entity}); 
+        return
     }
-     // Pas assez de PA pour atteindre le joueur.
-     else if let Some(path) = path_to_player {
-        if path.len() as u32  > npc_action_points.current {
-            forfeit = true;
-        } else {
-            ev_try_move.send(EntityTryMoveEvent {entity: entity, path: path.clone(), target: Some(player_position.v) });
-        }
-     };
+    println!("Planning: {:?} has a path to the target.", entity);
 
-     if forfeit {
-        ev_endturn.send(EntityEndTurnEvent {entity})
-     }
+    if let Some(path) = path_to_player {
+        // Est ce que je peux toucher le PJ?
+        if (path.len() as u32 * AP_COST_MOVE) + AP_COST_MELEE < npc_action_points.current {
+            println!("Planning: My target is close enough to hit: {:?}", entity);
+            ev_try_move.send(EntityTryMoveEvent { entity: entity, path: path.clone(), target: Some(player_position.v) });
+            return
+        }
+        // Si je ne peux pas le toucher, je m'approche quand même.
+        let mut new_path = VecDeque::new();
+        let mut ap = npc_action_points.current.clone();
+        let mut count = 0;
+        while ap > AP_COST_MOVE * 5 {
+            if let Some(new_position) = path.get(count) {
+                let next_position = new_position.clone();
+                count += 1;
+                ap -= 1;
+                new_path.push_back(next_position);
+            } else {
+                println!("Planning: Limite du path atteinte.");
+                break
+            }
+        }
+        if !new_path.is_empty() {
+            println!("Planning: Moving closer: {:?}", entity);
+            ev_try_move.send(EntityTryMoveEvent { entity: entity, path: new_path, target: Some(player_position.v) });
+            return
+        }
+    }
+    println!("Planning: {:?} has nothing better than forfeit.", {entity});
+    ev_endturn.send(EntityEndTurnEvent {entity})
 
 }
