11ada133baf5bfe667779615c0c1f5c443bb6da1
Kavekha <edelamaranthid@netcourrier.com>
2024-03-21 01:25:11 +0100

    Now we have mobs.
    Still need an exit and we are good.

--- src/game/manager/mod.rs
+++ src/game/manager/mod.rs
@@ -1,8 +1,8 @@
 use bevy::prelude::*;
 
-use crate::{game::{pieces::{components::Monster, spawners::{spawn_exit, spawn_npc, spawn_player}}, tileboard::components::BoardPosition}, map_builders::{map::Map, random_builder, BuilderMap}, vectors::Vector2Int};
+use crate::{game::{pieces::{components::Monster, spawners::{spawn_exit, spawn_npc, spawn_player, Kind}}, tileboard::components::BoardPosition}, map_builders::{map::Map, random_builder, BuilderMap}, vectors::Vector2Int};
 
-use super::{pieces::{components::{Health, Melee, Occupier, Piece, Stats}, spawners::get_random_kind}, player::Player, states::GameState};
+use super::{pieces::{components::{Health, Melee, Npc, Occupier, Piece, Stats, Walk}, spawners::get_random_kind}, player::Player, states::GameState};
 
 
 pub struct ManagerPlugin;
@@ -10,10 +10,6 @@ pub struct ManagerPlugin;
 impl Plugin for ManagerPlugin {
     fn build(&self, app: &mut App) {
         app
-        .insert_resource(GameInfos{
-            starting_position:Vector2Int { x:0, y: 0 },
-            spawn_list: Vec::new()
-        })   //Position Renommer 0.15.2
         .add_event::<MessageEvent>()   
         .add_systems(Update, handle_event.run_if(on_event::<MessageEvent>()));
     }
@@ -48,50 +44,13 @@ impl Message for TextMessage {
     }
 }
 
-pub struct CreatePlayerMessage;
-impl Message for CreatePlayerMessage {
-    fn execute(&self, world: &mut World) {
-        if let Some(game_infos) = world.get_resource::<GameInfos>(){
-            let player_starting_position = game_infos.starting_position;
-            println!("Player: Starting position = {:?}", player_starting_position);
-            let kind = get_random_kind();
-            let piece = Piece{kind: kind};
-
-            let mut player = world.spawn_empty();
-            
-            player
-                .insert(piece)
-                .insert(Player)
-                .insert(Name::new("The Shadowrunner"))
-                //TODO : Shadowrun stats
-                .insert(Stats {
-                    power: 3,         
-                    attack: 6,
-                    dodge: 6,
-                    resilience: 3
-                })
-                //.insert(Actor::default(),)
-                .insert(Health { max: 10, current: 10 })
-                .insert(Melee { damage: 1 })
-                .insert(BoardPosition{ v:player_starting_position })
-                .insert(Occupier);
-
-            world.send_event(MessageEvent(Box::new(TextMessage{source:"CreatePlayerMessage".to_string(), text:"Player has been created".to_string()})));
-        } else {
-            world.send_event(MessageEvent(Box::new(TextMessage{source:"CreatePlayerMessage".to_string(), text:"Player was NOT created [No Game Infos]".to_string()})));
-        };       
-
-
-    }
-}
-
-
 pub struct StartGameMessage;
 
 impl Message for StartGameMessage {
     fn execute(&self, world: &mut World) {
         let game_infos = create_map(world);
         create_player(world, game_infos.starting_position);
+        spawn_npcs(world, game_infos.spawn_list);
         world.send_event(MessageEvent(Box::new(DisplayMapMessage)));
     }
 }
@@ -104,18 +63,15 @@ fn create_map(world: &mut World) -> GameInfos {
 
         //get resource Game Infos
         let player_starting_position = builder.get_starting_position();  
-
-        //if let Some(game_infos) = world.get_resource::<GameInfos>(){
-            //let mut new_game_infos = game_infos.clone();
-            let mut new_game_infos = GameInfos{starting_position:Vector2Int{x:0, y:0}, spawn_list:Vec::new()};
-            new_game_infos.starting_position = player_starting_position;
-            world.insert_resource(new_game_infos.clone());
-            println!("Generating Map: Player starting position will be {:?}", player_starting_position);
+        let mut game_infos = GameInfos{starting_position:Vector2Int{x:0, y:0}, spawn_list:Vec::new()};
+        game_infos.starting_position = player_starting_position;
+        game_infos.spawn_list = builder.spawn_entities();
+        println!("Generating Map: Player starting position will be {:?}", player_starting_position);
         //};
         world.insert_resource(builder.build_data.map.clone());
 
         world.send_event(MessageEvent(Box::new(TextMessage{source:"CreateMapMessage".to_string(), text:"Map has been builded".to_string()})));
-        return new_game_infos
+        return game_infos
 }
 
 fn create_player(world: &mut World, player_starting_position: Vector2Int){
@@ -146,6 +102,38 @@ fn create_player(world: &mut World, player_starting_position: Vector2Int){
     //}
 }
 
+fn spawn_npcs(world: &mut World, entities_pos: Vec<Vector2Int>){
+    for entity_position in entities_pos {
+        println!("NPC: Starting position = {:?}", entity_position);
+        spawn_mob(world, entity_position);
+    }
+}
+
+fn spawn_mob(world: &mut World, npc_spawning_position: Vector2Int
+) {
+        let mut npc = world.spawn_empty();
+        
+        npc
+        .insert(Name::new(format!("Ghoul")))
+        .insert(Piece{kind: Kind::Ghoul })
+        .insert(Stats {
+            power: 4,         
+            attack: 4,
+            dodge: 3,
+            resilience: 4
+        })
+        //.insert(Actor::default(),)
+        .insert(Npc)
+        .insert(Monster)
+        .insert(Walk)
+        .insert(Melee { damage: 2 })
+        .insert(Health { max: 10, current: 10 })
+        .insert(BoardPosition{ v:npc_spawning_position })
+        .insert(Occupier);
+    println!("Npc created");
+}
+
+
 pub struct DisplayMapMessage;
 impl Message for DisplayMapMessage {
     fn execute(&self, world: &mut World) {
@@ -162,28 +150,6 @@ pub struct GameInfos{
     pub spawn_list: Vec<Vector2Int>
 }
 
-pub struct CreateMapMessage;
-impl Message for CreateMapMessage {
-    fn execute(&self, world: &mut World) {
-        println!("CreateMapMessage: Building Map.");
-        let mut builder = random_builder();
-        builder.build_map();        
-        builder.build_data.map.populate_blocked(); 
-
-        //get resource Game Infos
-        let player_starting_position = builder.get_starting_position();  
-
-        if let Some(game_infos) = world.get_resource::<GameInfos>(){
-            let mut new_game_infos = game_infos.clone();
-            new_game_infos.starting_position = player_starting_position;
-            world.insert_resource(new_game_infos.clone());
-            println!("Generating Map: Player starting position will be {:?}", player_starting_position);
-        };
-        world.insert_resource(builder.build_data.map.clone());
-
-        world.send_event(MessageEvent(Box::new(TextMessage{source:"CreateMapMessage".to_string(), text:"Map has been builded".to_string()})));
-
-    }
         /* TODO 
         if SHOW_MAPGEN_VISUALIZER {
             let mapgen_history = MapGenHistory{
@@ -219,5 +185,4 @@ impl Message for CreateMapMessage {
 
         commands.insert_resource(builder.build_data.map.clone());
 */
-}
-       
+     
