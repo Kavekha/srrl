afe8ddcd97b44741cd0abb03a16b0272200aeb72
Kavekha <edelamaranthid@netcourrier.com>
2024-03-26 20:18:26 +0100

    Rework spawning menu.

--- src/game/menus/menu_builder.rs
+++ src/game/menus/menu_builder.rs
@@ -10,11 +10,15 @@ use super::components::MenuButtonAction;
 
 
 //MenuBuilder v2
+#[derive(Clone)]
 pub struct Action {pub action: MenuButtonAction, pub text:String}
+#[derive(Clone)]
 pub struct Header {pub text: String}
+#[derive(Clone)]
 pub struct Description {pub text: String}
 
 
+#[derive(Clone)]
 pub enum MenuItem{
     Action(Action),
     Header(Header),
@@ -53,9 +57,137 @@ impl Menu{
     }
 }
 
-
-// Theme 2 : Recap Menu
+// WORK IN PROGRESS
 pub fn spawn_recap_menu(
+    commands: &mut Commands,
+    graph_assets: Res<GraphicsAssets>,
+    menu: &Menu
+) {
+    //let mut images:Vec<MenuItem> = Vec::new();
+    let mut headers:Vec<Header>= Vec::new();
+    let mut descriptions:Vec<Description> = Vec::new();
+    let mut actions:Vec<Action> = Vec::new();
+
+    for item in menu.entries.iter() {
+        match item {
+            MenuItem::Header(header) => headers.push(header.clone()),
+            MenuItem::Description(description) => descriptions.push(description.clone()),
+            MenuItem::Action(action) => actions.push(action.clone()),
+            _ => println!("This MenuItem is not supported.")
+        };
+    }
+
+    // Common style for all buttons on the screen
+    let button_style = Style {
+        width: Val::Px(125.0),
+        height: Val::Px(32.5),
+        margin: UiRect::all(Val::Px(10.0)),
+        justify_content: JustifyContent::Center,
+        align_items: AlignItems::Center,
+        ..default()
+    };
+    /*
+    let button_icon_style = Style {
+        width: Val::Px(30.0),
+        // This takes the icons out of the flexbox flow, to be positioned exactly
+        position_type: PositionType::Absolute,
+        // The icon will be close to the left border of the button
+        left: Val::Px(10.0),
+        ..default()
+    };
+        */
+    let button_text_style = TextStyle {
+        font_size: 20.0,    //40
+        color: TEXT_COLOR,
+        ..default()
+    };
+
+    // The full screen as a UI element
+    let screen_menu = commands.spawn(
+        (
+            NodeBundle {
+                style: Style {
+                    width: Val::Percent(100.0),
+                    height: Val::Percent(100.0),
+                    align_items: AlignItems::Center,
+                    justify_content: JustifyContent::Center,
+                    flex_direction: FlexDirection::Column,
+                    ..default()
+                },
+                ..default()
+            },
+            OnScreenMenu,
+        )).id();
+    
+    // Illustration.
+
+    // Header
+    for header in headers.iter() {
+        let menu_header = commands.spawn(TextBundle::from_section(
+            header.text.clone(),   //"YOU DIED.",
+            TextStyle {
+                font: graph_assets.font.clone(),
+                font_size: 30.0,
+                color: Color::rgb(1.0, 1.0, 1.0),
+            },
+        )).id();
+        commands.entity(screen_menu).push_children(&[menu_header]);
+    }
+
+    // Description
+    for description in descriptions.iter() {
+        let menu_description = commands.spawn(TextBundle::from_section(
+            description.text.clone(),   //"YOU DIED.",
+            TextStyle {
+                    font: graph_assets.font.clone(),
+                    font_size: 15.0,
+                    color: Color::rgb(1.0, 1.0, 1.0),
+                },
+            )).id();
+        commands.entity(screen_menu).push_children(&[menu_description]);
+    }
+
+    // Si y a des options, on mets un cadre.
+    if actions.len() > 0 {
+        let menu_border = commands.spawn(NodeBundle {
+            // Cadre du menu en lui-même.
+            style: Style {
+                flex_direction: FlexDirection::Column,
+                align_items: AlignItems::Center,
+                align_self: AlignSelf::Center,
+                ..default()
+            },
+            //background_color: Color::CRIMSON.into(),
+            ..default()
+        }).id();
+
+        commands.entity(screen_menu).push_children(&[menu_border]);
+
+        // Options.
+        for action in actions.iter() {
+            let action_button = commands.spawn((
+                ButtonBundle {
+                    style: button_style.clone(),
+                    background_color: NORMAL_BUTTON.into(),
+                    ..default()
+                },
+                action.action.clone(),    //action,
+            ))
+            .with_children(|parent| {
+                parent.spawn(TextBundle::from_section(
+                    action.text.clone(),  //text,
+                    button_text_style.clone(),
+                ));
+            }).id();
+            commands.entity(menu_border).push_children(&[action_button]);
+        };
+    }      
+}
+
+
+
+// Theme 2 : Recap Menu :: CURRENT
+pub fn spawn_recap_menu_2(
 commands: &mut Commands,
 graph_assets: Res<GraphicsAssets>,
 menu: &Menu
