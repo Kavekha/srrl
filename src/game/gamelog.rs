f56759cea12c9442c0340d937c1cff98cea4c3d0
Kavekha <edelamaranthid@netcourrier.com>
2024-03-28 08:47:37 +0100

    Add basic log event.

---
+++ src/game/gamelog.rs
@@ -0,0 +1,33 @@
+use bevy::prelude::*;
+
+pub struct GameLogsPlugin;
+
+impl Plugin for GameLogsPlugin {
+    fn build(&self, app: &mut App) {
+        app
+            .insert_resource(Gamelog { entries: Vec::new()})
+
+            .add_event::<LogEvent>()
+            
+            .add_systems(Update, display_log.run_if(on_event::<LogEvent>()))
+            ;
+    }
+}
+
+#[derive(Resource)]
+pub struct Gamelog {
+    pub entries : Vec<String>
+}
+
+#[derive(Event)]
+pub struct LogEvent{pub entry: String}
+
+fn display_log(
+    mut ev_log: EventReader<LogEvent>,
+    mut gamelog: ResMut<Gamelog>
+){
+    for event in ev_log.read() {
+        gamelog.entries.push(event.entry.to_string());
+        println!("{}", event.entry);
+    }    
+}
