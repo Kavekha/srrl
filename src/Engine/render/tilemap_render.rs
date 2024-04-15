3da9dd32f6422e5fad12edc10bc040d48cc0a50a
Kavekha <edelamaranthid@netcourrier.com>
2024-04-15 11:27:03 +0200

    fix missing rendering tiles.

--- src/Engine/render/tilemap_render.rs
+++ src/Engine/render/tilemap_render.rs
@@ -68,8 +68,97 @@ pub fn wall_corners(
 }
 
 
-// 0.20b : v2.
+
+// 0.20c : v2.
 pub fn spawn_map_render(
+    mut commands: Commands,
+    //asset_server: Res<AssetServer>,
+    assets: Res<GraphicsAssets>,
+    board: Res<Map>,
+    //tile_position_q: Query<(Entity, &BoardPosition), With<Tile>>,
+) {
+    println!("New rendering map begins...");
+
+    let mut dualgrid_floor = HashMap::new();
+    let mut dualgrid_wall = HashMap::new();
+
+    let mut floor_tiles:Vec<Entity> = Vec::new();
+    let mut wall_tiles:Vec<Entity> = Vec::new();
+
+    for (position, _entity) in board.entity_tiles.iter() {
+        // Comme on va avoir un décallage de grille, on doit gérer les angles pour ne pas generer plusieurs fois les tuiles graphiques.
+        // On ne peut pas le faire à la fin car elle ne nous est pas connue, il faut la faire au debut.
+        // Si x,y == 0,0  on genere le coté Nord Ouest de la tile 0,0. 
+        // Si x == 0, y!=0 on génère le coté Nord Est. 
+        // Si x != 0, y==0 on génère le coté Sud Ouest.
+        // Dans tous les autres cas, on génère systématiquement le coté Sud Est c'est à dire que x,y sera logic_x +1,logic_y +1.
+        // On récupère la position dans le monde pour cette position. 
+        let mut tiles_to_create = Vec::new();
+        if position.x == 0 && position.y == 0 {
+            tiles_to_create.push(Vector2Int { x: 0, y: 0 });
+            tiles_to_create.push(Vector2Int { x: 1, y: 0 });
+            tiles_to_create.push(Vector2Int { x: 0, y: 1 });
+        }
+        if position.x == 0 && position.y != 0 {
+            tiles_to_create.push(Vector2Int { x: 0, y: position.y });
+        } 
+        if position.y == 0 && position.x != 0 {
+            tiles_to_create.push(Vector2Int { x: position.x, y: 0 });
+        } 
+        // dans tous les cas: on genere la tuile graphique Sud Est.
+        tiles_to_create.push(Vector2Int { x: position.x + 1, y: position.y + 1 });
+    
+        for new_position in tiles_to_create {
+            let (mut world_x, mut world_y) = get_world_position(&new_position);
+            // On est sur la Dual Grid: Il faut un offset car le 0,0 logic est a cheval entre 0,0 - 0,1 - 1,0 - 1,1.
+            world_x -= (STANDARD_TILE_SIZE / 2) as f32;         
+            world_y -= (STANDARD_TILE_SIZE / 2) as f32; 
+
+            // On créé le sol 
+            let floor_tile = spawn_sprite_render(
+                &mut commands,
+                &assets.map_textures["floor"],
+                world_x,
+                world_y,
+                ORDER_FLOOR,
+                //MAP_FLOOR,
+            );
+            dualgrid_floor.insert(new_position.clone(), floor_tile);    // On insert la tuile Nord Ouest de la tuile logique x,y.
+            floor_tiles.push(floor_tile);
+            
+            // Si il y a un mur 
+            if let Some(texture) = wall_corners(&board, new_position.x, new_position.y) {
+                // Wall
+                let wall_tile = spawn_sprite_render(
+                    &mut commands,
+                    &assets.map_textures[texture], //&asset_server,  //&assets.textures["card"]
+                    world_x,
+                    world_y,
+                    ORDER_WALL,
+                    //texture,
+                );    
+                dualgrid_wall.insert(new_position.clone(), wall_tile); 
+                wall_tiles.push(wall_tile);
+            }
+        }
+    }
+    commands
+    .spawn(Name::new("Game Map Render"))
+    .insert(GameMapRender { 
+        floor: dualgrid_floor,
+        wall: dualgrid_wall
+    })
+    .insert(SpatialBundle{
+        ..default()
+    })
+    .push_children(&wall_tiles)
+    .push_children(&floor_tiles)
+    ;
+}
+
+
+// 0.20b : v2.
+pub fn spawn_map_render_old(
     mut commands: Commands,
     //asset_server: Res<AssetServer>,
     assets: Res<GraphicsAssets>,
