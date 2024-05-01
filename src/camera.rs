d584ec7829a8b442d75d3d94d9c55637b233bb4f
Kavekha <edelamaranthid@netcourrier.com>
2024-05-01 23:04:29 +0200

    Camera moving when cursor in corners.

--- src/camera.rs
+++ src/camera.rs
@@ -1,6 +1,9 @@
 use bevy::prelude::*;
 
-use crate::{engine::render::components::GameCursorRender, game::{player::Player, BASE_SPEED_CAMERA_SMOOTH_FOLLOW, SPEED_MULTIPLIER}};
+use crate::{engine::render::components::GameCursorRender, game::{player::{Cursor, Player}, SPEED_MULTIPLIER}, globals::CHAR_SIZE};
+
+pub const BASE_SPEED_CAMERA_SMOOTH_FOLLOW: f32 = 1.0;
+pub const BORDER_TOLERANCE: f32 = CHAR_SIZE * 10.0;  
 
 
 pub fn camera_center_on_player(
@@ -25,8 +28,42 @@ pub fn is_in_screen(
     return true
 }
 
-
 pub fn camera_smooth_follow(
+    mut camera_q: Query<(&Camera, &GlobalTransform, &mut Transform)>, 
+    time: Res<Time>,
+    res_cursor: Res<Cursor>,
+) {
+    let (camera, camera_gtransform, mut camera_transform) = camera_q.single_mut(); 
+    let Some(screen_size) = camera.logical_viewport_size() else { return };
+    let Some(screen_position) = camera.world_to_viewport(camera_gtransform, res_cursor.world_position) else { return };
+
+    println!("Screen size is {:?}. Cursor is at {:?}", screen_size, screen_position);
+
+    if screen_position.x > (screen_size.x - BORDER_TOLERANCE) {
+        camera_transform.translation.x = camera_transform.translation.x.lerp(
+            res_cursor.world_position.x * 0.1 + camera_transform.translation.x + 0.9, 
+            BASE_SPEED_CAMERA_SMOOTH_FOLLOW * SPEED_MULTIPLIER * time.delta_seconds())
+    }
+    if screen_position.x < (0.0 + BORDER_TOLERANCE) {
+        camera_transform.translation.x = camera_transform.translation.x.lerp(
+            0.0 + camera_transform.translation.x * 0.9, 
+            BASE_SPEED_CAMERA_SMOOTH_FOLLOW * SPEED_MULTIPLIER * time.delta_seconds())
+    }
+    if screen_position.y > (screen_size.y - BORDER_TOLERANCE) {
+        camera_transform.translation.y = camera_transform.translation.y.lerp(
+            res_cursor.world_position.y * 0.1 + camera_transform.translation.y + 0.9, 
+            BASE_SPEED_CAMERA_SMOOTH_FOLLOW * SPEED_MULTIPLIER * time.delta_seconds())
+    }
+    if screen_position.y < (0.0 + BORDER_TOLERANCE) {
+        camera_transform.translation.y = camera_transform.translation.y.lerp(
+            0.0 + camera_transform.translation.y * 0.9, 
+            BASE_SPEED_CAMERA_SMOOTH_FOLLOW * SPEED_MULTIPLIER * time.delta_seconds())
+    }
+}
+
+
+
+pub fn camera_smooth_follow_v1(
     player_query: Query<&Transform, With<Player>>,
     mut camera_query: Query<&mut Transform, (Without<Player>, With<Camera>)>,
     time: Res<Time>,
